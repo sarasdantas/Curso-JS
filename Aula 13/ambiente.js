b0d03f3e57a92d196a4a934eb4479b2b9570ec99
Sara S. Dantas <160888218+sarasdantas@users.noreply.github.com>
2024-05-07 10:52:23 -0300

    Aula 13

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+do { // fazer...
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+} while (c <= 6) // Enquanto c for...
+
+/*  
+var c = 1
+while (c <= 6) { // Enquanto c for <= 6...
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+*/
+
+/* console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
+
+
