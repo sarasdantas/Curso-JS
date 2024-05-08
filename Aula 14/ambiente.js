7f76e3e186a81eff595582c9673e6698e49a63e6
Sara S. Dantas <160888218+sarasdantas@users.noreply.github.com>
2024-05-08 10:28:05 -0300

    Aula 14 ex

---
+++ Aula 14/ambiente.js
@@ -0,0 +1,18 @@
+/*
+var c = 1
+do {
+    console.log(c)
+    c++
+} while (c <= 5)
+
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+*/
+
+// Inicialização é definir a variável, teste lógico é o que ele vai verificar se é true or false, e o incremento é o que vai adicionar à variável
+for (var c = 1; c <= 5; c++) {
+    console.log(c)
+}
