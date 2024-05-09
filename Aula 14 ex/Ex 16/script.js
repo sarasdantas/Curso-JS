1581ec9f95a935b04e0e24105284ee25f6b2c1bb
Sara S. Dantas <160888218+sarasdantas@users.noreply.github.com>
2024-05-09 16:38:27 -0300

    ex sozinhos

--- Aula 14 ex/Ex 16/script.js
+++ Aula 14 ex/Ex 16/script.js
@@ -1,1 +1,23 @@
-alert('olá')
+function contar() {
+    var txti = document.querySelector('input#inicio')
+    var txtf = document.querySelector('input#fim')
+    var txtp = document.querySelector('input#passo')
+    var res = document.querySelector('div#res')
+    var ini = Number(txti.value)
+    var fim = Number(txtf.value)
+    var pas = Number(txtp.value)
+
+
+
+    if (txti.length == 0 && txtf.length == 0 && txtp.length == 0) {
+        window.alert("Impossível contar, preencha todas as lacunas")
+    } else {
+        while (ini <= fim) {
+            res.innerHTML = ini
+            ini += pas        
+        }        
+    }
+
+
+
+}
--- Aula 14/ambiente.js
+++ Aula 14/ambiente.js
@@ -13,6 +13,18 @@ while (c <= 5) {
 */
 
 // Inicialização é definir a variável, teste lógico é o que ele vai verificar se é true or false, e o incremento é o que vai adicionar à variável
+/*
 for (var c = 1; c <= 5; c++) {
     console.log(c)
 }
+*/
+
+
+    var ini = 1
+    var fim = 5
+    var pas = 1
+
+    while (ini <= fim) {
+        console.log(ini) 
+        ini += pas        
+    }
