6bc7b9ddfc9d2720047e226554f867218a32ad50
Sara S. Dantas <160888218+sarasdantas@users.noreply.github.com>
2024-05-10 13:06:40 -0300

    ex modulo e

--- Aula 14 ex/Ex 16/script.js
+++ Aula 14 ex/Ex 16/script.js
@@ -1,23 +1,33 @@
 function contar() {
-    var txti = document.querySelector('input#inicio')
-    var txtf = document.querySelector('input#fim')
-    var txtp = document.querySelector('input#passo')
-    var res = document.querySelector('div#res')
-    var ini = Number(txti.value)
-    var fim = Number(txtf.value)
-    var pas = Number(txtp.value)
-
-
-
-    if (txti.length == 0 && txtf.length == 0 && txtp.length == 0) {
-        window.alert("Impossível contar, preencha todas as lacunas")
+    let ini = document.querySelector('input#inicio')
+    let fim = document.querySelector('input#fim')
+    let pas = document.querySelector('input#passo')
+    let res = document.querySelector('div#res')
+           
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
+        res.innerHTML = 'Impossível contar! Preencha todas as lacunas'
+        //window.alert("Impossível contar, preencha todas as lacunas")
     } else {
-        while (ini <= fim) {
-            res.innerHTML = ini
-            ini += pas        
-        }        
+        res.innerHTML = 'Contando: '  
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pas.value)
+        // Caso passo seja <= 0
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        //Contagem Crescente
+        if (i<f){
+            for (let c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            //Contagem Regressiva
+            for (let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
-
-
-
 }
--- Aula 14 ex/Ex 17/script.js
+++ Aula 14 ex/Ex 17/script.js
@@ -1,1 +1,23 @@
-alert('olá')
+function tabuada() {
+    let num = document.querySelector('input#txtn')
+    let tab = document.querySelector('select#seltab')
+    // Se não houver escrito o número
+    if (num.value.length == 0) {
+        alert('Por favor! Digite um número.')
+    } else {
+        // Altera o valor da variável para Number
+        let n = Number(num.value)
+        // Valor de c começa em 1
+        let c = 1
+        // Vai limpar o select antes de receber uma nova tabuada
+        tab.innerHTML = ''
+        // Tabuada
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            // Irá fazer o let item aparecer no select
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
